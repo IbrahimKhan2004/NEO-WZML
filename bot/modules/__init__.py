2ada959adc4d67155382ba4c8f2f0386f0357712
Ibrahim Khan <116390484+IbrahimKhan2004@users.noreply.github.com>
2026-09-12 19:51:19 +0530

    Add set_thumb to users_settings import

--- bot/modules/__init__.py
+++ bot/modules/__init__.py
@@ -35,7 +35,7 @@ from .services import start, start_cb, login, ping, log, log_cb
 from .shell import run_shell
 from .stats import bot_stats, stats_pages, get_packages_version
 from .status import task_status, status_pages
-from .users_settings import get_users_settings, edit_user_settings, send_user_settings
+from .users_settings import get_users_settings, edit_user_settings, send_user_settings, set_thumb
 from .ytdlp import ytdl, ytdl_leech
 from .speedtest import speedtest
 from . import dump_select
@@ -100,6 +100,7 @@ __all__ = [
     "get_users_settings",
     "edit_user_settings",
     "send_user_settings",
+    "set_thumb",
     "ytdl",
     "ytdl_leech",
     "speedtest",
