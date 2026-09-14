83bc955726e7a1696f859c642219c799f3f9b3a0
Ibrahim Khan <116390484+IbrahimKhan2004@users.noreply.github.com>
2026-09-15 01:49:39 +0530

    Add handler for removing stream selection

--- bot/core/handlers.py
+++ bot/core/handlers.py
@@ -203,6 +203,9 @@ def add_handlers():
 
     TgClient.bot.add_handler(CallbackQueryHandler(edit_video_tool, filters=regex("^vt")))
     TgClient.bot.add_handler(
+        CallbackQueryHandler(edit_remove_stream_selection, filters=regex("^rss"))
+    )
+    TgClient.bot.add_handler(
         MessageHandler(
             uphoster,
             filters=command(BotCommands.UpHosterCommand, case_sensitive=True)
