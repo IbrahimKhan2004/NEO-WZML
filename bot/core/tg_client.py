2abcc49e272e6efdabcc922f84911c1c3039541f
Ibrahim Khan <116390484+IbrahimKhan2004@users.noreply.github.com>
2026-09-18 23:27:11 +0530

    Increase max concurrent transmissions from 12 to 16

--- bot/core/tg_client.py
+++ bot/core/tg_client.py
@@ -31,7 +31,7 @@ class TgClient:
         kwargs["parse_mode"] = enums.ParseMode.HTML
         kwargs["in_memory"] = True
         for param, value in {
-            "max_concurrent_transmissions": 12,
+            "max_concurrent_transmissions": 16,
             "skip_updates": False,
         }.items():
             if param in signature(Client.__init__).parameters:
