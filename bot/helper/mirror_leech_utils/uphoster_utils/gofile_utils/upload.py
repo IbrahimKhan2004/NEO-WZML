08e754ce85961a9bf70b00ada4f285bca06a4f0e
Ibrahim Khan <116390484+IbrahimKhan2004@users.noreply.github.com>
2026-09-21 04:55:48 +0530

    Fix error handling in upload.py

--- bot/helper/mirror_leech_utils/uphoster_utils/gofile_utils/upload.py
+++ bot/helper/mirror_leech_utils/uphoster_utils/gofile_utils/upload.py
@@ -350,7 +350,7 @@ class GoFileUpload:
             if isinstance(err, RetryError):
                 LOGGER.info(f"Total Attempts: {err.last_attempt.attempt_number}")
                 err = err.last_attempt.exception()
-            err = str(err).replace(">", "").replace("<", "")
+            err = str(err).replace(">", "").replace("<", "") or type(err).__name__
             LOGGER.error(err)
             await self.listener.on_upload_error(err)
             self._is_errored = True
